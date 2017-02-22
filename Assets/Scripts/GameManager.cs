0972c9bca240625b447106ea9ddeb03428d08e31
SAE\ighadban <1006878@student.sae.edu.au>
2017-02-22 20:57:42 +1000

    cleaned up some code

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using System.Net;
 
 public class GameManager : MonoBehaviour {
 
@@ -13,6 +14,11 @@ public class GameManager : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
+
+        //WebClient webClient = new WebClient();
+        //webClient.DownloadFile("https://drive.google.com/uc?export=download&id=0B2tthDH6GnuULTNqamF1SkpNRHM", "testing.csv");
+
+
         LoadCubes();
 
     }
@@ -32,11 +38,9 @@ public class GameManager : MonoBehaviour {
 
     }
 
-    void SaveCubes()
-    {
+    void SaveCubes() {
         StreamWriter sw = new StreamWriter("cubelocation.csv");          //  "d:\\test.csv" Application.persistentDataPath + "/cubelocation.csv"
-        foreach (GameObject cubes in cubes0)
-        {
+        foreach (GameObject cubes in cubes0) {
             sw.Write("Cube0");
             sw.Write(",");
             sw.Write(cubes.transform.position.x);
@@ -48,8 +52,7 @@ public class GameManager : MonoBehaviour {
             sw.Write(cubes.transform.eulerAngles.y);
             sw.Write("\n");
         }
-        foreach (GameObject cubes in cubes1)
-        {
+        foreach (GameObject cubes in cubes1) {
             sw.Write("Cube1");
             sw.Write(",");
             sw.Write(cubes.transform.position.x);
@@ -61,8 +64,7 @@ public class GameManager : MonoBehaviour {
             sw.Write(cubes.transform.eulerAngles.y);
             sw.Write("\n");
         }
-        foreach (GameObject cubes in cubes2)
-        {
+        foreach (GameObject cubes in cubes2) {
             sw.Write("Cube2");
             sw.Write(",");
             sw.Write(cubes.transform.position.x);
@@ -74,8 +76,7 @@ public class GameManager : MonoBehaviour {
             sw.Write(cubes.transform.eulerAngles.y);
             sw.Write("\n");
         }
-        foreach (GameObject cubes in cubes3)
-        {
+        foreach (GameObject cubes in cubes3) {
             sw.Write("Cube3");
             sw.Write(",");
             sw.Write(cubes.transform.position.x);
@@ -91,15 +92,13 @@ public class GameManager : MonoBehaviour {
         //sw.Flush();
 
     }
-    void LoadCubes()
-    {
+    void LoadCubes() {
         StreamReader sr = new StreamReader("cubelocation.csv");    //"d:\\test.csv" Application.persistentDataPath + "/cubelocation.csv" 
         Vector3 pos = new Vector3();
         float angle = 0.0f;
         string type;
                 
-        while (!sr.EndOfStream)
-        {
+        while (!sr.EndOfStream) {
             string s = sr.ReadLine();
             string[] sa = s.Split(',');
             type = (sa[0]);
@@ -108,20 +107,16 @@ public class GameManager : MonoBehaviour {
             pos.z = float.Parse(sa[3]);
             angle = float.Parse(sa[4]);
 
-            if (type == "Cube0")
-            {
+            if (type == "Cube0") {
                 Instantiate(GameObject.FindGameObjectWithTag("Player").GetComponent<Player>().cube0, pos, Quaternion.Euler(0.0f, angle, 0.0f));
             }
-            if (type == "Cube1")
-            {
+            if (type == "Cube1") {
                 Instantiate(GameObject.FindGameObjectWithTag("Player").GetComponent<Player>().cube1, pos, Quaternion.Euler(0.0f, angle, 0.0f));
             }
-            if (type == "Cube2")
-            {
+            if (type == "Cube2") {
                 Instantiate(GameObject.FindGameObjectWithTag("Player").GetComponent<Player>().cube2, pos, Quaternion.Euler(0.0f, angle, 0.0f));
             }
-            if (type == "Cube3")
-            {
+            if (type == "Cube3") {
                 Instantiate(GameObject.FindGameObjectWithTag("Player").GetComponent<Player>().cube3, pos, Quaternion.Euler(0.0f, angle, 0.0f));
             }
 
